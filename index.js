3c4d6c74bb9d6b26acadc9288f07856be19e702b
Kevin Isom <kevin.isom@gmail.com>
2015-03-19 13:06:04 +1300

    tidy up and fix

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-module.exports ={ 
-    'use strict';
+'use strict';
+module.exports = {
     get: function (opts) {
-        var endpoint = opts.endpoint, node = opts.node, data = opts.data, querystring, qstring;
+        console.log(typeof opts);
+        var endpoint, node, data, querystring, qstring;
+        if (typeof opts === 'string') {
+            endpoint = opts;
+        } else {
+            endpoint = opts.endpoint;
+            node = opts.node;
+            data = opts.data; 
+        }
+        
         if (data !== null) {
             querystring = require('querystring');
             qstring = querystring.stringify(data);
             endpoint = endpoint + '?' + qstring;
         }
         var q = new XMLHttpRequest();
+        console.log(q);
         q.open('GET', endpoint, false);
         q.send(null);
-        return JSON.parse(q.response)[node];
+        console.log(q.response);
+        if (node) {
+           return JSON.parse(q.response)[node]; 
+        }
+        return JSON.parse(q.response);
     }
 };
